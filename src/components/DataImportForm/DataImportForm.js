63f2a40051afc055e83d92fe3db5c748db0738bb
W <warren@warrendaviesdev.com>
2022-04-30 16:20:52 +0100

    added DataImportForm component

---
+++ src/components/DataImportForm/DataImportForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DataImportForm.scss'
+
+export default function DataImportForm() {
+    return (
+        <div className='DataImportForm'>
+            Click here to import your data ->
+        </div>
+    )
+}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.scss'
+import DataImportForm from '../DataImportForm/DataImportForm'
 
 export default function Header (props) {
     return (
         <header className='Header'>
-
+            <h1 className='Header__text'>Your Spotify Data</h1>
+            <DataImportForm />
         </header>
     )
 }
