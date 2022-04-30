e13299fb92f209f58e78535f7a055d763cb2b7e0
W <warren@warrendaviesdev.com>
2022-04-30 15:00:21 +0100

    added sidebar component and dashboard container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className='container'>
-
+        <Sidebar />
+        <div className='dashboard'>
+          
+        </div>
       </div>
     </div>
   );
