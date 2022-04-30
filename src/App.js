3a349fbf7cf724e71abd80e71821cb68146e8bce
W <warren@warrendaviesdev.com>
2022-04-30 14:38:54 +0100

    added sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Header from './components/Header/Header'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Header />
       <div className='container'>
-        
+
       </div>
     </div>
   );
