73594cb7ca359bac17ff3894aad77653f959a229
W <warren@warrendaviesdev.com>
2022-06-03 10:23:20 +0100

    added search filter to artists table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,46 +1,66 @@
-import React from 'react';
-import { useTable } from 'react-table';
+import React, {useState} from 'react';
+import { useTable, useFilters } from 'react-table';
 
 export default function Table({columns, data}) {
     
+    const [filterInput, setFilterInput] = useState('');
+
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
         rows,
-        prepareRow
-    } = useTable({
-        columns,
-        data
-    })
+        prepareRow,
+        setFilter
+    } = useTable(
+        {
+            columns,
+            data
+        },
+        useFilters
+    )
+
+    const handleFilterChange = e => {
+        const value = e.target.value || '';
+        setFilter('artistName', value)
+        setFilterInput(value);
+    }
 
     return (
-        <table {...getTableProps()}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                        <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <td {...cell.getCellProps()}>
-                                        {cell.render("Cell")}
-                                    </td>
-                                )   
-                            })}
+        <div id='artistTable'>
+            <input
+                value={filterInput} 
+                onChange={handleFilterChange}
+                placeholder={'Search for an artist'}
+            />
+            
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                            ))}
                         </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render("Cell")}
+                                        </td>
+                                    )   
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
