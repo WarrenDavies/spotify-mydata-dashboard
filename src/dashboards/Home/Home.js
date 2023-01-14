47833aee88ba2502a984dffe94a78d297b735ea8
W <warren@warrendaviesdev.com>
2023-01-14 20:05:45 +0000

    copied time chart to home page

--- src/dashboards/Home/Home.js
+++ src/dashboards/Home/Home.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
+import Table from '../../components/Table/Table';
+import { Link } from 'react-router-dom';
+import * as d3 from 'd3';
+import BarChart from '../../components/vis/BarChart/BarChart';
+import * as dateAndTime from '../../utils/DateAndTime';
 
 
 
 export default function Home(props) {
 
     const [listensProcessed, setListensProcessed] = useState(0)
+
+    const timeChart = {}
+    timeChart.width = 960;
+    timeChart.height = 700;
+    timeChart.margin = { top: 20, right: 20, bottom: 20, left: 230};
+    timeChart.innerHeight = timeChart.height - timeChart.margin.top - timeChart.margin.bottom - 100;
+    timeChart.innerWidth = timeChart.width - timeChart.margin.left - timeChart.margin.right;
+    timeChart.xAxisLabelOffset = 50
+    timeChart.xValue = d => d.dateOfListen;
+    timeChart.yValue = d => d.hrsPlayed;
+    timeChart.d3Format = d3.format(".2s")
+    timeChart.xAxisTickFormat = n => timeChart.d3Format(n)
 
     function processData(data) {
         // let listensUploaded;
@@ -30,8 +47,31 @@ export default function Home(props) {
             data.length: {props.data.length} <br/>
             listensProcessed: {listensProcessed} <br/><br/>
             
+            <h1>Listening History over Time</h1>
+
+            <BarChart 
+                width={timeChart.width}
+                height={timeChart.height}
+                innerHeight={timeChart.innerHeight}
+                innerWidth={timeChart.innerWidth}
+                margin={timeChart.margin}
+                data={props.stats.time.dates}
+                xValue={timeChart.xValue}
+                yValue={timeChart.yValue}
+                xAxisLabelOffset={timeChart.xAxisLabelOffset}
+                xAxisTickFormat={timeChart.xAxisTickFormat}
+            />
+
+
+            <h1>Top Artists</h1>
+
+
+
             data: {JSON.stringify(props.data)} <br/>
             
+            
+
+
         </div>
     )
 }
