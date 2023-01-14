b3dd9c7d5ecebdaef42c85f7e655167858eb4b1e
W <warren@warrendaviesdev.com>
2023-01-14 20:20:46 +0000

    copied top artists chart to home page

--- src/dashboards/Home/Home.js
+++ src/dashboards/Home/Home.js
@@ -4,12 +4,23 @@ import { Link } from 'react-router-dom';
 import * as d3 from 'd3';
 import BarChart from '../../components/vis/BarChart/BarChart';
 import * as dateAndTime from '../../utils/DateAndTime';
-
+import BarChartHorizontalCategorical from '../../components/vis/BarChart/BarChartHorizontalCategorical';
+import ReactDropdown from 'react-dropdown';
 
 
 export default function Home(props) {
 
     const [listensProcessed, setListensProcessed] = useState(0)
+    const [artistData, updateArtistData] = useState(props.stats.artists);
+
+    // msPlayed/listens dropdown
+    const dropDownAttributes = [
+        { value: 'hrsPlayed', label: 'Listening time (hours)' },
+        { value: 'uniqueListens', label: 'Number of listens' }
+    ];
+    const initialBarChartMeasure = 'hrsPlayed';
+    const [barChartMeasure, setBarChartMeasure] = useState(initialBarChartMeasure);
+
 
     const timeChart = {}
     timeChart.width = 960;
@@ -19,9 +30,42 @@ export default function Home(props) {
     timeChart.innerWidth = timeChart.width - timeChart.margin.left - timeChart.margin.right;
     timeChart.xAxisLabelOffset = 50
     timeChart.xValue = d => d.dateOfListen;
-    timeChart.yValue = d => d.hrsPlayed;
+    timeChart.yValue = d => d[barChartMeasure];
     timeChart.d3Format = d3.format(".2s")
     timeChart.xAxisTickFormat = n => timeChart.d3Format(n)
+
+    const topArtistChart = {}
+    topArtistChart.width = 960;
+    topArtistChart.height = 700;
+    topArtistChart.margin = { top: 20, right: 20, bottom: 20, left: 230 };
+    topArtistChart.innerHeight = topArtistChart.height - topArtistChart.margin.top - topArtistChart.margin.bottom - 100;
+    topArtistChart.innerWidth = topArtistChart.width - topArtistChart.margin.left - topArtistChart.margin.right;
+    topArtistChart.xAxisLabelOffset = 50
+    topArtistChart.xValue = d => d[barChartMeasure];
+    topArtistChart.yValue = d => d.artistName;
+    topArtistChart.d3Format = d3.format(".2s")
+    topArtistChart.xAxisTickFormat = n => topArtistChart.d3Format(n)
+
+    const topArtists = useMemo(() => {
+        const hrsPlayed = props.stats.artists
+            .sort((a, b) => {
+                return b.hrsPlayed - a.hrsPlayed;
+            })
+            .slice(0, 19);
+
+        const uniqueListens = props.stats.artists
+            .sort((a, b) => {
+                return b.uniqueListens - a.uniqueListens;
+            })
+            .slice(0, 19);
+
+        return (
+            {
+                "hrsPlayed": hrsPlayed,
+                "uniqueListens": uniqueListens
+            }
+        )
+    }, [props.stats.artists])
 
     function processData(data) {
         // let listensUploaded;
@@ -37,6 +81,14 @@ export default function Home(props) {
         processData(props.data);
     });
     
+    if (!props.data) {
+        return (
+            <>
+                Upload some data!
+            </>
+        )
+    }
+
     return (
         
         <div className='Home'>
@@ -46,6 +98,13 @@ export default function Home(props) {
 
             data.length: {props.data.length} <br/>
             listensProcessed: {listensProcessed} <br/><br/>
+
+            <ReactDropdown
+                options={dropDownAttributes}
+                onChange={option => setBarChartMeasure(option.value)}
+                value={initialBarChartMeasure}
+                dropdownLabel="Choose time or listens: "
+            />
             
             <h1>Listening History over Time</h1>
 
@@ -65,7 +124,18 @@ export default function Home(props) {
 
             <h1>Top Artists</h1>
 
-
+            <BarChartHorizontalCategorical
+                width={topArtistChart.width}
+                height={topArtistChart.height}
+                innerHeight={topArtistChart.innerHeight}
+                innerWidth={topArtistChart.innerWidth}
+                margin={topArtistChart.margin}
+                data={topArtists[barChartMeasure]}
+                xValue={topArtistChart.xValue}
+                yValue={topArtistChart.yValue}
+                xAxisLabelOffset={topArtistChart.xAxisLabelOffset}
+                xAxisTickFormat={topArtistChart.xAxisTickFormat}
+            />
 
             data: {JSON.stringify(props.data)} <br/>
             
