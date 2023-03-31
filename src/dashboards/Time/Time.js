df42bb992774e9a30bb9d702fa20102f8d6265c9
W <warren@warrendaviesdev.com>
2023-03-31 12:39:23 +0100

    Added some styling to chart and headline stats on time page

--- src/dashboards/Time/Time.js
+++ src/dashboards/Time/Time.js
@@ -4,8 +4,34 @@ import { Link } from 'react-router-dom';
 import * as d3 from 'd3';
 import BarChart from '../../components/vis/BarChart/BarChart';
 import * as dateAndTime from '../../utils/DateAndTime';
+import StatBoxContainer from '../../components/vis/StatBox/StatBoxContainer';
 
 export default function Time(props) {
+    // Listens Uploaded = {props.listensUploaded}<br/>
+    // Days listened on = {props.stats.time.dates.length}<br/>
+    // From: {props.stats.highLevel.minDate.toDateString()}<br/>
+    // To: {props.stats.highLevel.maxDate.toDateString()}<br/>
+    // Days in period: {dateAndTime.convertMsToDays(props.stats.highLevel.daysInPeriod)}<br/>
+
+    const headlineStats = [
+        {
+            header: 'Total listening time',
+            stat: props.stats.highLevel.totalListeningTimeString
+        },
+        {
+            header: 'Total unique Listens',
+            stat: props.listensUploaded
+        },
+        {
+            header: 'Days listened on',
+            stat: props.stats.time.dates.length
+        },
+        {
+            header: 'Days in period',
+            stat: dateAndTime.convertMsToDays(props.stats.highLevel.daysInPeriod),
+        },
+    ]
+
 
     const columns = useMemo(() => [
         {
@@ -55,32 +81,61 @@ export default function Time(props) {
         }
     ])
 
+
     // bar chart
 
-    const width = 960;
-    const height = 700;
-    const margin = { top: 20, right: 20, bottom: 20, left: 230};
+    const width = 1400;
+    const height = 600;
+    const margin = { top: 20, right: 20, bottom: 20, left: 50};
     const innerHeight = height - margin.top - margin.bottom - 100;
     const innerWidth = width - margin.left - margin.right;
     const xAxisLabelOffset = 50
     const xValue = d => d.dateOfListen;
     const yValue = d => d.hrsPlayed;
-    const d3Format = d3.format(".2s")
-    const xAxisTickFormat = n => d3Format(n)
+    const d3Format = d3.format(".2s");
+    const xAxisTickFormat = n => d3Format(n);
 
+    const timeChart = {
+        width: 1400,
+        height: 600,
+        margin: { top: 20, right: 50, bottom: 20, left: 50 },
+        xAxisOffset: 10,
+        xAxisLabel: 'Date',
+        xAxisLabelOffset: 23,
+        xValue: d => d.dateOfListen,
+        yValue: d => d.hrsPlayed,
+        d3Format: d3.format(".2s"),
+        xAxisTickFormat: n => timeChart.d3Format(n),
+    }
+    timeChart.innerHeight = timeChart.height - timeChart.margin.top - timeChart.margin.bottom - 100;
+    timeChart.innerWidth = timeChart.width - timeChart.margin.left - timeChart.margin.right;
+
+    if (!props.data) {
+
+        headlineStats.forEach((j) => {
+            j.stat = 0;
+        })
+
+        return (
+            <>
+                Upload some data!
+            </>
+        )
+    }
 
     return (
         
         <div className='Time'>
             
             Time dashboard<br/>
-            Listens Uploaded = {props.listensUploaded}<br/>
-            Days listened on = {props.stats.time.dates.length}<br/>
-            From: {props.stats.highLevel.minDate.toDateString()}<br/>
-            To: {props.stats.highLevel.maxDate.toDateString()}<br/>
-            Days in period: {dateAndTime.convertMsToDays(props.stats.highLevel.daysInPeriod)}<br/>
+
+
+            <StatBoxContainer 
+                statBoxes={headlineStats}
+            />
 
             <BarChart 
+                id="time-bar-chart"
                 width={width}
                 height={height}
                 innerHeight={innerHeight}
@@ -89,7 +144,9 @@ export default function Time(props) {
                 data={props.stats.time.dates}
                 xValue={xValue}
                 yValue={yValue}
+                xAxisLabel={timeChart.xAxisLabel}
                 xAxisLabelOffset={xAxisLabelOffset}
+                xAxisOffset={timeChart.xAxisOffset}
                 xAxisTickFormat={xAxisTickFormat}
             />
 
